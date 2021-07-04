ad472a2f931fd317e4f50680c285783d8e08c344
KienDA <doanhkien001@gmail.com>
2021-07-04 10:03:24 +0700

    update the name

--- hotelmanagerment/src/test/java/com/example/hotelmanagerment/PokemonApplicationTests.java
+++ hotelmanagerment/src/test/java/com/example/hotelmanagerment/PokemonApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HotelmanagermentApplicationTests {
+class PokemonApplicationTests {
 
     @Test
     void contextLoads() {
