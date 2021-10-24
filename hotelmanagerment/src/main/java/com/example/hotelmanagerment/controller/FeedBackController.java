9d81d7fcdefa911afbda61b627df9542bc6ca2ba
DoAnhKien <doanhkien001@gmail.com>
2021-10-24 13:30:15 +0700

    update my task

--- hotelmanagerment/src/main/java/com/example/hotelmanagerment/controller/FeedBackController.java
+++ hotelmanagerment/src/main/java/com/example/hotelmanagerment/controller/FeedBackController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-    @RequestMapping("/feedback")
+@RequestMapping("/feedback")
 public class FeedBackController {
 
     @Autowired
